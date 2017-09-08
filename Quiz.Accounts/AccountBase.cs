adb27bea56f26f0d15de72e2a22285fc48f267da
Andrew Wu <chicken@chicken-house.net>
2017-09-08 10:19:42 +0800

    remove answer

--- Quiz.Accounts/AccountBase.cs
+++ Quiz.Accounts/AccountBase.cs
@@ -37,34 +37,7 @@ namespace Quiz.Accounts
                 if (total != 0) return false;
             }
 
-
-
-            //
-            // NOTE: 協調多個帳號之間的交易機制，主要是要靠兩階段提交 (two phase commit) 的機制來解決。第一階段要協調每個交易的參與者，
-            // 必須在一定的時間內明確回覆能否同意這項交易? 協調者在一定時間內確認每個參與者都同意的話，第二階段就要開始執行交易。
-            // 到了第二階段，就必須確認交易已經成功為止。若沒有接到回應或確認，則必須不斷重試。
-            //
-            // 如果你無意間發現這個 commit, 歡迎參考我的實作版本。不過在你要抄這份 code 前請先想清楚，在面試時你必須要能
-            // 清楚解釋你寫的 code 才行!
-            //
-            // Andrew Wu
-            //
-            {
-                // phase 1, make sure transaction execution
-                foreach (TransactionCmd tc in transes)
-                {
-                    if ((tc.account.GetBalance() + tc.amount) < 0) return false;
-                }
-            }
-
-            {
-                // phase 2, execute transactions until success
-                foreach (TransactionCmd tc in transes)
-                {
-                    DoTransferUntilSuccess(tc.account, tc.amount);
-                }
-                return true;
-            }
+            throw new NotImplementedException();
         }
 
         private static void DoTransferUntilSuccess(AccountBase from, long amount)
